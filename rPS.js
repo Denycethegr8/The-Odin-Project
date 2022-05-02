55510c33ffbcadefcab561dfe916e6cfc5665e94
denyce <jini3209@gmail.com>
2022-05-02 22:06:23 +0900

    simplify code

--- rPS.js
+++ rPS.js
@@ -1,27 +1,15 @@
 function computerPlay(){
-    let which = [0, 1, 2];
-    return which[Math.floor(Math.random()*3)];
+    if (Math.floor(Math.random()*3) === 0){
+        return 'rock';
+    } else if (Math.floor(Math.random()*3) === 1){
+        return 'paper';
+    } else return 'scissors';
 }
+
+
+let player = prompt('Rock? Paper? Scissors?')
 
 //to randomly generates rock(0), paper(1), scissors(2)//
-
-
-let question = prompt('Rock? Paper? Scissors?').toLowerCase()
-
-//to get question from the user in lower case//
-
-function change2Int(){
-    let input = question;
-    if (input === 'rock') {
-        return 0;
-    } else if (input === 'paper') {
-        return 1;
-    } else {
-        return 2;
-    }
-}
-
-//to change the user's input to number corresponding to 0, 1, 2 as computer's//
 
 
 function oneRound(playerSelection, computerSelection){
@@ -36,10 +24,5 @@ function oneRound(playerSelection, computerSelection){
 
 //to compare the number converted from user's input with the computer's //
 
-
-let playerSelection = change2Int();
-let computerSelection = computerPlay();
-
-console.log(oneRound(playerSelection, computerSelection));
-
+console.log(oneRound(player, computerPlay()));
 
