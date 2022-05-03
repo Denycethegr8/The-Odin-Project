9753730cd52c3228a07a01be3baea599472acf3d
denyce <jini3209@gmail.com>
2022-05-03 22:32:47 +0900

    update one round game to n rounds of games

--- rPS.js
+++ rPS.js
@@ -1,28 +1,48 @@
-function computerPlay(){
+
+//randomly generate rock(0), paper(1), scissors(2)
+
+let computerPlay = function(){
+    let result = ''
     if (Math.floor(Math.random()*3) === 0){
-        return 'rock';
+        result = 'rock';
     } else if (Math.floor(Math.random()*3) === 1){
-        return 'paper';
-    } else return 'scissors';
+        result = 'paper';
+    } else {
+        result = 'scissors';
+    } return result;
 }
 
+//one round of rock paper scissors
 
-let player = prompt('Rock? Paper? Scissors?')
-
-//to randomly generates rock(0), paper(1), scissors(2)//
-
-
+let playerScore = 0;
+let computerScore = 0;
 function oneRound(playerSelection, computerSelection){
     if (playerSelection === computerSelection){
-        return 'Draw!';
-    } else if (playerSelection - computerSelection === 1 || playerSelection - computerSelection === -2) {
-        return 'You win!';
+        console.log('Draw!');
+    } else if (playerSelection === 'rock' && computerSelection === 'scissors' || playerSelection === 'scissors' && computerSelection === 'paper' || playerSelection === 'paper' && computerSelection === 'rock') {
+        console.log('You win!');
+        playerScore++;
     } else { 
-        return 'I win!';
+        console.log('I win!');
+        computerScore++;
     }
 }
 
-//to compare the number converted from user's input with the computer's //
 
-console.log(oneRound(player, computerPlay()));
+//play game n times
 
+function multipleRounds(n){
+    for (let i = 0; i < n; i++){
+        oneRound(prompt('Rock? Paper? Scissors?').toLowerCase(), computerPlay())
+    } 
+    if (playerScore === computerScore){
+        return 'Draw! Good game!'
+    } else if (playerScore > computerScore){
+        return 'You win! Congrats!'
+    } else return 'I win! Good game!'
+}
+
+
+//ask user how many rounds of game they would like to play before run the function
+
+multipleRounds(prompt('How many games would you like to play?'));
